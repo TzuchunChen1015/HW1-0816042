8bab2e7532b367ab6e0f2a244532895e06a223b2
TzuchunChen1015 <tzuchunchen1015@gmail.com>
2022-10-09 12:04:52 +0800

    Add Select FD and SendMsg Function to server.cpp

--- server.cpp
+++ server.cpp
@@ -6,10 +6,13 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <sys/time.h>
-#include <sys/types.>
 #include <unistd.h>
+#include <set>
+#include <vector>
 using namespace std;
+const int MXL = 1e3;
 string RecvMsg(int);
+void SendMsg(int, string, bool);
 int main(int argc, char** argv) {
 	// Set Server IP and Port	
 	const string IP = argv[1];
@@ -34,8 +37,44 @@ int main(int argc, char** argv) {
 	}	
 	bind(udpFd, (const struct sockaddr*) &serverAddr, sizeof(serverAddr));
 	// Use Select
-	FD_SET rSet, allSet;
-	
+	fd_set rSet, allSet; int mxFd = max(tcpFd, udpFd) + 1;
+	FD_SET(tcpFd, &allSet);
+	FD_SET(udpFd, &allSet);
+	set<int> clientSet; set<int>::iterator it; vector<int> rmSet;
+	while(true) {
+		rSet = allSet;
+		select(mxFd, &rSet, NULL, NULL, NULL);
+		if(FD_ISSET(tcpFd, &rSet)) {
+			int clientFd = accept(tcpFd, NULL, NULL);
+			FD_SET(clientFd, &allSet);
+			mxFd = max(mxFd, clientFd + 1);
+			clientSet.insert(clientFd);
+			SendMsg(clientFd, "*****Welcome to Game 1A2B*****\n", 0);
+			SendMsg(clientFd, "% ", 1);
+		}
+		if(FD_ISSET(udpFd, &rSet)) {
+			
+		} 
+		rmSet.clear();
+		for(it = clientSet.begin(); it != clientSet.end(); it++) {
+			int clientFd = *it;
+			if(FD_ISSET(clientFd, &rSet)) {
+				string msg = RecvMsg(clientFd);
+				if(msg == "") rmSet.push_back(clientFd);
+				else {
+					SendMsg(clientFd, "Your Sent " + msg + " to the Server\n", 0);
+					SendMsg(clientFd, "% ", 1);
+				}
+			}
+		}
+		// Remove Closed Clients
+		for(int i = 0; i < (int) rmSet.size(); i++) {
+			FD_CLR(rmSet[i], &allSet);
+			clientSet.erase(rmSet[i]);
+			close(rmSet[i]);
+			cout << "Number " << rmSet[i] << " disconnected\n";
+		}	
+	}
 	return 0;
 }
 string RecvMsg(int fd) {
@@ -46,4 +85,14 @@ string RecvMsg(int fd) {
 		msg += BUF[0];
 	}
 	return msg;
+}
+void SendMsg(int fd, string s, bool lastMsg) {
+	char BUF[MXL + 1];
+	strcpy(BUF, s.c_str()); BUF[s.length()] = '\0';
+	int leftBytes = s.length() + lastMsg, now = 0, nBytes;
+	while(leftBytes) {
+		nBytes = send(fd, BUF + now, leftBytes, 0);
+		leftBytes -= nBytes;
+		now += nBytes;
+	}
 }
