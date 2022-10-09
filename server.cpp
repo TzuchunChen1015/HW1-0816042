95a8a65093b02d8f83bff3591045f1063e5bc0b2
TzuchunChen1015 <tzuchunchen1015@gmail.com>
2022-10-09 14:23:25 +0800

    Modify makefile; Add login, logout and exit Functions for server.cpp

--- server.cpp
+++ server.cpp
@@ -9,10 +9,24 @@
 #include <unistd.h>
 #include <set>
 #include <vector>
+#include <sstream>
+#include <map>
 using namespace std;
-const int MXL = 1e3;
+const int MXL = 1e3, MXClient = 1e3;
+struct UserInfo { string name, email, password; };
+map<string, UserInfo> nameMap, emailMap;
+struct ClientStatus {
+	bool login = false;
+	string username;
+} CS[MXClient + 1];
+vector<int> rmSet;
 string RecvMsg(int);
 void SendMsg(int, string, bool);
+void DealWithCommand(int, string);
+void CommandLogin(int, vector<string>&);
+void CommandLogout(int, vector<string>&);
+void CommandStartGame(int, vector<string>&);
+void CommandExit(int, vector<string>&);
 int main(int argc, char** argv) {
 	// Set Server IP and Port	
 	const string IP = argv[1];
@@ -38,12 +52,14 @@ int main(int argc, char** argv) {
 	bind(udpFd, (const struct sockaddr*) &serverAddr, sizeof(serverAddr));
 	// Use Select
 	fd_set rSet, allSet; int mxFd = max(tcpFd, udpFd) + 1;
+	FD_ZERO(&allSet);
 	FD_SET(tcpFd, &allSet);
 	FD_SET(udpFd, &allSet);
-	set<int> clientSet; set<int>::iterator it; vector<int> rmSet;
+	set<int> clientSet; set<int>::iterator it;
 	while(true) {
 		rSet = allSet;
 		select(mxFd, &rSet, NULL, NULL, NULL);
+		// Listen and Accept
 		if(FD_ISSET(tcpFd, &rSet)) {
 			int clientFd = accept(tcpFd, NULL, NULL);
 			FD_SET(clientFd, &allSet);
@@ -52,9 +68,11 @@ int main(int argc, char** argv) {
 			SendMsg(clientFd, "*****Welcome to Game 1A2B*****\n", 0);
 			SendMsg(clientFd, "% ", 1);
 		}
+		// Handle UDP Message
 		if(FD_ISSET(udpFd, &rSet)) {
 			
-		} 
+		}
+		// Check Each Client 
 		rmSet.clear();
 		for(it = clientSet.begin(); it != clientSet.end(); it++) {
 			int clientFd = *it;
@@ -62,13 +80,16 @@ int main(int argc, char** argv) {
 				string msg = RecvMsg(clientFd);
 				if(msg == "") rmSet.push_back(clientFd);
 				else {
-					SendMsg(clientFd, "Your Sent " + msg + " to the Server\n", 0);
-					SendMsg(clientFd, "% ", 1);
+					msg.pop_back();
+					DealWithCommand(clientFd, msg);
+					if(rmSet.empty() || rmSet.back() != clientFd)
+						SendMsg(clientFd, "% ", 1);
 				}
 			}
 		}
 		// Remove Closed Clients
 		for(int i = 0; i < (int) rmSet.size(); i++) {
+			CS[rmSet[i]].login = false;
 			FD_CLR(rmSet[i], &allSet);
 			clientSet.erase(rmSet[i]);
 			close(rmSet[i]);
@@ -81,8 +102,8 @@ string RecvMsg(int fd) {
 	string msg; char BUF[2]; int nBytes;
 	BUF[1] = '\0';
 	while((nBytes = recv(fd, BUF, 1, 0)) > 0) {
-		if(BUF[0] == '\n') break;
 		msg += BUF[0];
+		if(BUF[0] == '\n') break;
 	}
 	return msg;
 }
@@ -95,4 +116,39 @@ void SendMsg(int fd, string s, bool lastMsg) {
 		leftBytes -= nBytes;
 		now += nBytes;
 	}
+}
+void DealWithCommand(int fd, string s) {
+	if(!s.length()) return;
+	stringstream ss; ss << s;
+	string str; vector<string> v;
+	while(ss >> str) v.push_back(str);
+	if(v.front() == "login") CommandLogin(fd, v);
+	else if(v.front() == "logout") CommandLogout(fd, v);
+	else if(v.front() == "start-game") CommandStartGame(fd, v);
+	else if(v.front() == "exit") CommandExit(fd, v);
+	else SendMsg(fd, "ERROR: Command Not Found\n", 0);
+}
+void CommandLogin(int fd, vector<string>& v) {
+	if(v.size() != 3) SendMsg(fd, "Usage: login <username> <password>\n", 0);
+	else if(CS[fd].login) SendMsg(fd, "Please logout first.\n", 0);
+	else if(nameMap.find(v[1]) == nameMap.end()) SendMsg(fd, "Username not found.\n", 0);
+	else if(v[2] != nameMap[v[1]].password) SendMsg(fd, "Password not correct.\n", 0);
+	else {
+		CS[fd].login = true;
+		CS[fd].username = v[1];
+		SendMsg(fd, "Welcome, " + v[1] + ".\n", 0);
+	}
+}
+void CommandLogout(int fd, vector<string>& v) {
+	if(v.size() != 1) SendMsg(fd, "Usage: logout\n", 0);
+	else if(!CS[fd].login) SendMsg(fd, "Please login first.\n", 0);
+	else {
+		CS[fd].login = false;
+		SendMsg(fd, "Bye, " + CS[fd].username + ".\n", 0);
+	}
+}
+void CommandStartGame(int fd, vector<string>& v) {}
+void CommandExit(int fd, vector<string>& v) {
+	if(v.size() != 1) SendMsg(fd, "Usage: exit\n", 0);
+	else rmSet.push_back(fd);
 }
